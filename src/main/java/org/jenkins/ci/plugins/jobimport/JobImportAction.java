66b17d16ecd2f108912351ddb2e15a637520b0a8
Jesse Farinacci <jieryn@gmail.com>
2011-07-20 10:14:25 -0400

    need help.. pushing all changes

--- src/main/java/org/jenkins/ci/plugins/jobimport/JobImportAction.java
+++ src/main/java/org/jenkins/ci/plugins/jobimport/JobImportAction.java
@@ -26,16 +26,26 @@ package org.jenkins.ci.plugins.jobimport;
 
 import hudson.Extension;
 import hudson.model.RootAction;
+import hudson.util.FormValidation;
 
 import java.io.IOException;
+import java.net.MalformedURLException;
+import java.util.Arrays;
+import java.util.Map;
+import java.util.Set;
 import java.util.SortedSet;
 import java.util.TreeSet;
 import java.util.logging.Logger;
 
 import javax.servlet.ServletException;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.xpath.XPathExpressionException;
 
+import org.apache.commons.lang.builder.ToStringBuilder;
+import org.kohsuke.stapler.QueryParameter;
 import org.kohsuke.stapler.StaplerRequest;
 import org.kohsuke.stapler.StaplerResponse;
+import org.xml.sax.SAXException;
 
 /**
  * @author <a href="mailto:jieryn@gmail.com">Jesse Farinacci</a>
@@ -46,16 +56,43 @@ public final class JobImportAction implements RootAction {
 
   private static final Logger LOG = Logger.getLogger(JobImportAction.class.getName());
 
-  public void doQuery(final StaplerRequest request, final StaplerResponse response) throws ServletException,
-      IOException {
-    LOG.info("doQuery(request, response)");
-    response.forwardToPreviousPage(request);
+  public static void main(final String[] args) {
+    try {
+      System.out.println(RemoteJobUtils.fromXml(URLUtils.fetchUrl("http://localhost:8080/view/ViewName/api/xml")));
+    }
+
+    catch (final Exception e) {
+      e.printStackTrace();
+    }
   }
 
+  @SuppressWarnings({ "rawtypes", "unchecked" })
   public void doImport(final StaplerRequest request, final StaplerResponse response) throws ServletException,
       IOException {
     LOG.info("doImport(request, response)");
+
+    for (final Map.Entry parameter : (Set<Map.Entry>) request.getParameterMap().entrySet()) {
+      LOG.info(parameter.getKey() + " -> " + ToStringBuilder.reflectionToString(parameter.getValue()));
+    }
+
+    if (request.hasParameter("jobUrl")) {
+      final SortedSet<String> jobUrls = new TreeSet<String>(Arrays.asList(request.getParameterValues("jobUrl")));
+
+    }
+
     response.forwardToPreviousPage(request);
+  }
+
+  public void doQuery(final StaplerRequest request, final StaplerResponse response) throws ServletException,
+      IOException {
+    LOG.info("doQuery(request, response)");
+    LOG.info(request.getParameter("remoteUrl"));
+
+    response.forwardToPreviousPage(request);
+  }
+
+  public FormValidation doTestConnection(@QueryParameter("remoteUrl") final String remoteUrl) {
+    return FormValidation.ok();
   }
 
   public String getDisplayName() {
@@ -66,31 +103,31 @@ public final class JobImportAction implements RootAction {
     return "/images/32x32/setting.png";
   }
 
-  public boolean isRemoteJobsAvailable() {
-    return getRemoteJobs().size() > 0;
-  }
+  public SortedSet<RemoteJob> getRemoteJobs(/*
+                                             * @QueryParameter("remoteUrl")
+                                             * final String remoteUrl
+                                             */) throws XPathExpressionException, MalformedURLException, SAXException,
+      IOException, ParserConfigurationException {
+    // final SortedSet<RemoteJob> remoteJobs = new TreeSet<RemoteJob>();
+    //
+    // remoteJobs.add(new RemoteJob("Job 1",
+    // "http://ci.jenkins-ci.org/job/Job1", "Job 1 is an example job."));
+    // remoteJobs.add(new RemoteJob("Job 2",
+    // "http://ci.jenkins-ci.org/job/Job2", "Job 2 is an example job."));
+    // remoteJobs.add(new RemoteJob("Job 3",
+    // "http://ci.jenkins-ci.org/job/Job3", "Job 3 is an example job."));
+    //
+    // return remoteJobs;
 
-  public SortedSet<RemoteJob> getRemoteJobs() {
-    final SortedSet<RemoteJob> remoteJobs = new TreeSet<RemoteJob>();
-
-    remoteJobs.add(new RemoteJob("Job 1", "http://ci.jenkins-ci.org/job/Job1", "Job 1 is an example job."));
-    remoteJobs.add(new RemoteJob("Job 2", "http://ci.jenkins-ci.org/job/Job2", "Job 2 is an example job."));
-    remoteJobs.add(new RemoteJob("Job 3", "http://ci.jenkins-ci.org/job/Job3", "Job 3 is an example job."));
-
-    return remoteJobs;
+    LOG.info("getRemoteJobs(" /* + remoteUrl */+ ")");
+    return RemoteJobUtils.fromXml(URLUtils.fetchUrl(/* remoteUrl + */"/api/xml"));
   }
 
   public String getUrlName() {
     return "/job-import";
   }
 
-  public static void main(final String[] args) {
-    try {
-      System.out.println(RemoteJobUtils.fromXml(URLUtils.fetchUrl("http://localhost:8080/view/ViewName/api/xml")));
-    }
-
-    catch (final Exception e) {
-      e.printStackTrace();
-    }
+  public boolean isRemoteJobsAvailable() {
+    return true;// getRemoteJobs().size() > 0;
   }
 }
