a356f5bb36b656ff9f206be88834921a56ebeb95
Emilio Escobar <escoem@users.noreply.github.com>
2018-05-30 11:35:38 +0200

    still using documentfactory

--- src/main/java/org/jenkins/ci/plugins/jobimport/client/RestApiClient.java
+++ src/main/java/org/jenkins/ci/plugins/jobimport/client/RestApiClient.java
@@ -26,7 +26,12 @@ public final class RestApiClient {
         List<RemoteItem> items = new ArrayList<>();
         try {
             if (StringUtils.isNotEmpty(url)) {
-                Document doc = DocumentBuilderFactory.newInstance().newDocumentBuilder().parse(
+                DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+                factory.setFeature("http://xml.org/sax/features/external-general-entities", false);
+                factory.setFeature("http://xml.org/sax/features/external-parameter-entities", false);
+                factory.setFeature("http://apache.org/xml/features/disallow-doctype-decl", true);
+                factory.setFeature("http://apache.org/xml/features/nonvalidating/load-external-dtd", false);
+                Document doc = factory.newDocumentBuilder().parse(
                         URLUtils.fetchUrl(URLUtils.safeURL(url , Constants.XML_API_QUERY), credentials.username, credentials.password));
                 NodeList nl = doc.getElementsByTagName("job");
 
